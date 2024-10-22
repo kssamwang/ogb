2bd91f0c089dd4d6118ba7b6a598671a7742e168
Shangyou Wang <51863110+kssamwang@users.noreply.github.com>
2024-10-22 14:37:53 +0800

    Update version.py
    disable version check

--- ogb/version.py
+++ ogb/version.py
@@ -4,24 +4,25 @@ from threading import Thread
 
 __version__ = '1.3.6'
 
-try:
-    os.environ['OUTDATED_IGNORE'] = '1'
-    from outdated import check_outdated  # noqa
-except ImportError:
-    check_outdated = None
+# NOTE: DISABLE Version Check !!!!! The thread may result in deadloack when the program is quitting
+# try:
+#     os.environ['OUTDATED_IGNORE'] = '1'
+#     from outdated import check_outdated  # noqa
+# except ImportError:
+#     check_outdated = None
 
 
-def check():
-    try:
-        is_outdated, latest = check_outdated('ogb', __version__)
-        if is_outdated:
-            logging.warning(
-                f'The OGB package is out of date. Your version is '
-                f'{__version__}, while the latest version is {latest}.')
-    except Exception:
-        pass
+# def check():
+#     try:
+#         is_outdated, latest = check_outdated('ogb', __version__)
+#         if is_outdated:
+#             logging.warning(
+#                 f'The OGB package is out of date. Your version is '
+#                 f'{__version__}, while the latest version is {latest}.')
+#     except Exception:
+#         pass
 
 
-if check_outdated is not None:
-    thread = Thread(target=check)
-    thread.start()
+# if check_outdated is not None:
+#     thread = Thread(target=check)
+#     thread.start()
